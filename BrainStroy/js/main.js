88e2baa72d8ee59c42d3a30cef45de83721d9c1a
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-05-11 17:14:43 +0700

    Блок технологии + анимации и стрелка вверх
    Добавил блок технологии и адаптировал его + добавил простую анимацию с rotate на карточки и кнопку,листающую страницу вверх

--- BrainStroy/js/main.js
+++ BrainStroy/js/main.js
@@ -2,3 +2,35 @@
       document.querySelector(".headerMain__menu").classList.toggle("visibleMain");
       document.querySelector(".headerMain__burger").classList.toggle("close");
   }
+ 
+
+let card = document.querySelectorAll(".technology__item");
+
+for(let i = 0;i<card.length;i++){
+    card[i].addEventListener("mousemove",rotate);
+    card[i].addEventListener("mouseout",rotateStop);
+}
+
+
+function rotate(event){
+    let cardItem = this.querySelector(".technology__card");
+    let halfHeight = cardItem.offsetHeight / 2;
+    cardItem.style.transform = "rotateX("+ -(event.offsetY - halfHeight) / 20+"deg) rotateY("+(event.offsetX - halfHeight) / 20+"deg)"
+    console.log(event.offsetY);
+}
+function rotateStop(event){
+    let cardItem = this.querySelector(".technology__card");
+    cardItem.style.transform = "rotate(0)";
+}
+
+$(function() {
+ 
+$(window).scroll(function() {
+($(this).scrollTop() != 0) ? $('#toTop').fadeIn() :  $('#toTop').fadeOut(); 
+});
+
+$('#toTop').click(function() {
+$('*').animate({scrollTop:0},500);
+});
+
+});
