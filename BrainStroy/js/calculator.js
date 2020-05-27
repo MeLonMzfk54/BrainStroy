cf91cdaf5831ebbfe8f07e0365b54050be67b296
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-05-27 22:08:57 +0700

    доделал калькулятор
    доделал калькулятор

--- BrainStroy/js/calculator.js
+++ BrainStroy/js/calculator.js
@@ -1,12 +1,50 @@
 $(document).ready(function(){
-   $('.calculator__text').click(function(event){
+   $('.calculator__text_m').click(function(event){
        if($('.calculator__item').hasClass('one')){
-           $('.calculator__text').not($(this)).removeClass('active');
+           $('.calculator__text_m').not($(this)).removeClass('active');
            $('.calculator__subdesc').not($(this).next()).slideToggle(300);
        }
        $(this).toggleClass("active").next().slideToggle(300);
    });
-    
-    
-   
+    $(".calculator__button").click(function(){
+        let valueMat = $(".calculator__radio:checked").val(); // материал
+        let valueKomp = $(".calculator__radio_r:checked").val(); // комплектация
+        let valueSquare = $(".calculator__input").val(); // площадь
+//        alert(valueMat); alert(valueKomp);alert(valueSquare);
+        if(valueMat == "wood"){
+            if(valueKomp == "eco"){
+                let result = valueSquare * 16000;
+                $(".calculator__cost").html(result);
+            }
+            if(valueKomp == "standard"){
+                let result = valueSquare * 20000;
+                $(".calculator__cost").html(result);
+            }
+            if(valueKomp == "premium"){
+                let result = valueSquare * 25000;
+                $(".calculator__cost").html(result);
+            }
+        }
+        if(valueMat == "brick"){
+            if(valueKomp == "eco"){
+                let result = valueSquare * 20000;
+                $(".calculator__cost").html(result);
+            }
+            if(valueKomp == "standard"){
+                let result = valueSquare * 25000;
+                $(".calculator__cost").html(result);
+            }
+            if(valueKomp == "premium"){
+                let result = valueSquare * 31000;
+                $(".calculator__cost").html(result);
+            }
+        }
+    });
 });
+
+function numbersOnly(){
+        if(event.keyCode != 43 && event.keyCode <48 || event.keyCode>57){
+            event.preventDefault();
+        }
+    }
+
