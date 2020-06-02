f428c32b9a1a2e795ffb30e68ed18525f43e1325
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-06-02 20:04:15 +0700

    Страницы для домов
    Страницы для домов с адаптацией, без картинок

--- BrainStroy/js/buttons.js
+++ BrainStroy/js/buttons.js
@@ -9,4 +9,14 @@ $(".preview__btn").click(function(){
     });
 });
 
+$(".house__btn").click(function(){
+    $(".popup").css({'top':250+"px"}).addClass("active__popup");
+    $(".popup__bg").fadeIn();
+    $(".popup__title").html("Подать заявку");
+    $(".popup__bg").click(function(){
+        $(".popup").removeClass("active__popup");
+        $(".popup__bg").fadeOut();
+    });
+});
+
 
